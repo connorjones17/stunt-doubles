dbf14099c8073fc80cc41ba0e22d8d9c16bdb233
connorjones17 <connor.la.jones@gmail.com>
2022-12-01 03:16:57 +0300

    Add types to client API

--- src/api/UserClient.ts
+++ src/api/UserClient.ts
@@ -1,17 +1,18 @@
 import {userStore, workerStore} from '../store/store';
+import {Worker} from '../store/types';
 
-export const getUnviewedWorkers = () => {
+export const getUnviewedWorkers = (): Worker[] => {
   const workers = workerStore.getAll();
   const viewed = allViewedWorkers();
 
   return workers.filter(worker => !viewed.includes(worker.id));
 };
 
-export const userlikesWorker = (id: string) => {
+export const userlikesWorker = (id: string): void => {
   userStore.like(id);
 };
 
-export const userDislikesWorker = (id: string) => {
+export const userDislikesWorker = (id: string): void => {
   userStore.dislike(id);
 };
 
