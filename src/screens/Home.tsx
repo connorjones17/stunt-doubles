603b7d9270f599c2f1a8ef958883de7ec6147048
connorjones17 <connor.la.jones@gmail.com>
2022-12-01 03:06:52 +0300

    Setup basic App and Homepage
    The intial build of the App and the Homescreen page. Keeping them separate at the moment in case we introduce routing, redux etc down the line. These will be useful as a starting point for initialisation

---
+++ src/screens/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SwipeWorkers from '../components/workers/SwipeWorkers';
+
+const HomeScreen = () => {
+  return <SwipeWorkers />;
+};
+
+export default HomeScreen;
